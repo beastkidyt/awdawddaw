6267aba0800f7e79b0a8ba02a64d2d43284324da
beastkidyt <71515479+beastkidyt@users.noreply.github.com>
2020-09-19 10:45:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.js');
 var botName = require('./package.json');
-var bot = new Discord.Client();
+var bot = new Discord.Client(756393213400711268);
 
 bot.on('message', message =>{
 
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "756393213400711268" && message.channel.id === "756476134710968321"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
+            if(msg === prefix + "bang" && message.channel.id === "756476134710968321"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
